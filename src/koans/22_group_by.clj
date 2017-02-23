cedbf0d220fdf6f141106d28da9a76f45883a8d5
Vinay <vinayv@thoughtworks.com>
2017-02-23 22:08:06 +0530

    Finished upto level 22

--- src/koans/22_group_by.clj
+++ src/koans/22_group_by.clj
@@ -7,7 +7,7 @@
 
 (meditations
   "To categorize a collection by some function, use group-by."
-  (= __ (group-by count ["hello" "world" "foo" "bar"]))
+  (= 4 (group-by count ["hello" "world" "foo" "bar"]))
 
   "You can simulate filter + remove in one pass"
   (= (get-odds-and-evens [1 2 3 4 5])
